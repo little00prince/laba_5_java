a24fcff317f5e58d06ec99b6a6079e124c532f0f
«Anton» <antonaryy98@gmail.com>
2024-09-23 21:32:31 +0700

    Добавил для класса triangular_prism защищенный блок, который не дает создавать невозможное основание в виде несуществующего треугольника

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int i, j = 0, k = 0;
+        int i, f, j = 0, k = 0;
         double e, s, v;
         double[] sides = new double[3];
         cube one = null;
@@ -9,83 +9,97 @@ public class Main {
         tetrapyramid[] three = new tetrapyramid[2];
         triangular_prism four = null;
         Scanner in = new Scanner(System.in);
-        System.out.print("\n1-Прямоугольный параллелепипед\n2-Шар\n3-Правильный тетраэдр\n4-Треугольная призма\n--------------------\nВыберите фигуру:");
-        figure.setType(in.nextInt());
-        s = 0;
-        v = 0;
-        //Ввод и обработка
-        switch (figure.type) {
-            case 1:
-                System.out.print("Длины сторон прямоугольного параллелепипеда:");
-                for (i = 0; i < 3; i++) {
-                    sides[i] = in.nextDouble();
-                }
-                one = new cube(sides);
-                one.workCube();
-                break;
-            case 2:
-                System.out.print("Радиус шара:");
-                e = in.nextDouble();
-                two[j][k] = new ball(e);
-                two[j][k].workBall();
-                break;
-            case 3:
-                System.out.print("Длина стороны правильного тетраэдра:");
-                e = in.nextDouble();
-                three[j] = new tetrapyramid(e);
-                three[j].workTetrapyramid();
-                break;
-            case 4:
-                System.out.print("Длины сторон Треугольной призмы:");
-                for (i = 0; i < 3; i++) {
-                    sides[i] = in.nextDouble();
-                }
-                four = new triangular_prism(sides);
-                four.workе_triangular_prism();
-                break;
-            default:;
-        }
-        //Вывод результатов
-        System.out.print("Фигура: ");
-        switch (figure.type) {
-            case 1:
-                i = one.getView();
-                String view = null;
-                switch (i) {
+        do {
+            System.out.print("\n1-Прямоугольный параллелепипед\n2-Шар\n3-Правильный тетраэдр\n4-Треугольная призма\n--------------------\nВыберите фигуру:");
+            figure.setType(in.nextInt());
+            f = 1;
+            s = 0;
+            v = 0;
+            //Ввод и обработка
+            switch (figure.type) {
+                case 1:
+                    System.out.print("Длины сторон прямоугольного параллелепипеда:");
+                    for (i = 0; i < 3; i++) {
+                        sides[i] = in.nextDouble();
+                    }
+                    one = new cube(sides);
+                    one.workCube();
+                    break;
+                case 2:
+                    System.out.print("Радиус шара:");
+                    e = in.nextDouble();
+                    two[j][k] = new ball(e);
+                    two[j][k].workBall();
+                    break;
+                case 3:
+                    System.out.print("Длина стороны правильного тетраэдра:");
+                    e = in.nextDouble();
+                    three[j] = new tetrapyramid(e);
+                    three[j].workTetrapyramid();
+                    break;
+                case 4:
+                    System.out.print("Длины сторон Треугольной призмы:");
+                    for (i = 0; i < 3; i++) {
+                        sides[i] = in.nextDouble();
+                    }
+                    four = new triangular_prism(sides);
+                    try {
+                        four.workе_triangular_prism();
+                    } catch (Exception ex) {
+                        System.out.println(ex.getMessage());
+                        f = 0;
+                    }
+                    break;
+                default:
+                    ;
+            }
+            //Вывод результатов
+            if (f == 1) {
+                System.out.print("Фигура: ");
+                switch (figure.type) {
                     case 1:
-                        view = "Куб";
+                        i = one.getView();
+                        String view = null;
+                        switch (i) {
+                            case 1:
+                                view = "Куб";
+                                break;
+                            case 2:
+                                view = "Прямоугольный параллелепипед";
+                                break;
+                            default:
+                                ;
+                        }
+                        System.out.print(view);
+                        e = one.getDiagonal();
+                        System.out.printf("\nДиагональ: %f", e);
+                        s = one.getArea();
+                        v = one.getVolume();
                         break;
                     case 2:
-                        view = "Прямоугольный параллелепипед";
+                        System.out.println("Шар");
+                        e = two[j][k].getD();
+                        System.out.printf("Диаметр: %f", e);
+                        s = two[j][k].getArea();
+                        v = two[j][k].getVolume();
                         break;
-                    default:
-                        ;
+                    case 3:
+                        System.out.print("Правильный тетраэдр");
+                        s = three[j].getArea();
+                        v = three[j].getVolume();
+                        break;
+                    case 4:
+                        System.out.println("Треугольная призма");
+                        s = four.getArea();
+                        v = four.getVolume();
+                        break;
                 }
-                System.out.print(view);
-                e = one.getDiagonal();
-                System.out.printf("\nДиагональ: %f", e);
-                s = one.getArea();
-                v = one.getVolume();
-                break;
-            case 2:
-                System.out.println("Шар");
-                e = two[j][k].getD();
-                System.out.printf("Диаметр: %f", e);
-                s = two[j][k].getArea();
-                v = two[j][k].getVolume();
-                break;
-            case 3:
-                System.out.print("Правильный тетраэдр");
-                s = three[j].getArea();
-                v = three[j].getVolume();
-                break;
-            case 4:
-                System.out.println("Треугольная призма");
-                s = four.getArea();
-                v = four.getVolume();
-                break;
+                System.out.printf("\nПлощадь: %f", s);
+                System.out.printf("\nОбъем: %f\n", v);
+            }
+            k++;
+            j++;
         }
-        System.out.printf("\nПлощадь: %f", s);
-        System.out.printf("\nОбъем: %f\n", v);
+        while(k < 2);
     }
 }
--- src/triangular_prism.java
+++ src/triangular_prism.java
@@ -5,11 +5,15 @@ public class triangular_prism extends figure{
         System.arraycopy(sides, 0, this.sides, 0, 3);
     }
 
-    public void workе_triangular_prism(){
+    public void workе_triangular_prism()throws Exception{
         double p;
         h = 1;
         p = (sides[0] + sides[1] + sides[2]) / 2;
         area = Math.sqrt(p * (p - sides[0]) * (p - sides[1]) * (p - sides[2])) + h * (sides[0] + sides[1] + sides[2]);
         volume = Math.sqrt(p * (p - sides[0]) * (p - sides[1]) * (p - sides[2])) * h;
+        if ((sides[0] >= sides[1] + sides[2]) || (sides[1] >= sides[0] + sides[2]) || (sides[2] >= sides[0] + sides[1]))
+        {
+            throw new Exception ("Треугольная призма не существует");
+        }
     }
 }
